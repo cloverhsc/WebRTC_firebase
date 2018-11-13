0f187fb6d98157e41fe7f40da6bc1e92ccb7dd4f
clover <clover.hsc@gmail.com>
2018-11-13 11:22:24 +0800

    update README.md and remove firebase initialize config.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,14 @@
 
 export const environment = {
   production: false,
+  // input your firebase project Initialize config.
   firebase: {
-    apiKey: 'AIzaSyBly4huhhQ5nmO36ZPrnNrRO_QpG4lFySM',
-    authDomain: 'webrtc-testconn.firebaseapp.com',
-    databaseURL: 'https://webrtc-testconn.firebaseio.com',
-    projectId: 'webrtc-testconn',
-    storageBucket: 'webrtc-testconn.appspot.com',
-    messagingSenderId: '808723277909'
+    apiKey: 'xxxxxxxxxxxxx',
+    authDomain: 'xxxxxxxxxxxxx',
+    databaseURL: 'https://xxx.xxx.xxx',
+    projectId: 'yyy-yyyyyy',
+    storageBucket: 'xxxxx.yyyy.zzz',
+    messagingSenderId: '123456789'
   }
 };
 
